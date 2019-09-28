fd43b26cd4f407bb573953ba26bc919b3737ef30
mushtaqj <mjmushtaq@gmail.com>
2019-09-28 17:00:35 +0530

    Remove bloatware from xiaomi app

---
+++ xioami-redmi5plus.bat
@@ -0,0 +1,53 @@
+REM google duo
+pm uninstall -k --user 0 com.google.android.apps.tachyon 
+
+REM google photos
+pm uninstall -k --user 0 com.google.android.apps.photos 
+
+REM play music
+pm uninstall -k --user 0 com.google.android.music
+
+REM play movies
+pm uninstall -k --user 0 com.google.android.videos  
+
+REM xiaomi browser
+pm uninstall -k --user 0 com.android.browser
+
+REM xiaomi messenger
+pm uninstall -k --user 0 com.android.mms
+
+REM xiaomi mi pay
+pm uninstall -k --user 0 com.xiaomi.payment
+
+REM xiaomi mi share
+pm uninstall -k --user 0 com.xiaomi.midrop
+
+REM xiaomi mi pay
+pm uninstall -k --user 0 com.miui.hybrid
+
+REM gboard keyboard
+pm uninstall -k --user 0 com.google.android.inputmethod.latin
+
+REM facebook services
+pm uninstall -k --user 0 com.facebook.services
+
+REM xiaomi app store
+pm uninstall -k --user 0 com.xiaomi.mipicks
+
+REM xiaomi cloud service
+pm uninstall -k --user 0 com.miui.cloudservice
+
+REM xiaomi cloud backup
+pm uninstall -k --user 0 com.miui.cloudbackup
+
+REM facebook install prompter
+pm uninstall -k --user 0 com.facebook.system
+
+REM xiaomi wallet
+pm uninstall -k --user 0 com.mipay.wallet.in
+
+REM xiaomi share ux 
+pm uninstall -k --user 0 com.miui.bugreport
+
+REM xiaomi scanner
+pm uninstall -k --user 0 com.xiaomi.scanner
